9b64c9d11c3a74058337970eb32f0b9b66edddfd
Bril Mikhail <Kolabrod@mail.ru>
2020-04-13 20:58:41 +0300

    Updates

---
+++ precache-manifest.4da23094b3d19cf1c04d0a2e0992e648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6005ca93e94f2db603a70f266bbd75db",
+    "url": "/tic-tac-toe-REACT/index.html"
+  },
+  {
+    "revision": "43602e534e8f55527c39",
+    "url": "/tic-tac-toe-REACT/static/css/main.e96b8795.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/tic-tac-toe-REACT/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-REACT/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43602e534e8f55527c39",
+    "url": "/tic-tac-toe-REACT/static/js/main.60c6918e.chunk.js"
+  },
+  {
+    "revision": "d8637d3baa9653f67db3",
+    "url": "/tic-tac-toe-REACT/static/js/runtime-main.cd78c1c4.js"
+  }
+]);
